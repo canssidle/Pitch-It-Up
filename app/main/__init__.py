edb64d6353a5ce338776846106747cd8d49ea1cd
canssidle wairimu <canssidlewairimu@gmail.com>
2018-05-22 14:28:33 +0300

    registered blueprint

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,8 +1,19 @@
 
 from flask import Blueprint
 main = Blueprint('main',__name__)
-
-
 from . import views,errors
 
+def create_app(config_name):
+    app = Flask(__name__)
 
+    app.config.from_object(config_options[config_name])
+
+    #Initializing flask extensions
+
+    bootstrap.init_app(app)
+
+    #Registering a blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
