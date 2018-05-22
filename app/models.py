4792f2f767788d43d1aac729cc4aeb8c74f1b47b
canssidle wairimu <canssidlewairimu@gmail.com>
2018-05-22 11:12:11 +0300

    updated user class

--- app/models.py
+++ app/models.py
@@ -1,15 +1,22 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+now = datetime.now()
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
 
-    
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(225))
+    username = db.Column(db.String(225),index = True)
+    email = db.Column(db.String(225),unique = True,index = True)
+    bio = db.Column(db.String(225))
+    password_hash = db.Column(db.String(225))
 
 
     def __repr__(self):
--- manage.py
+++ manage.py
@@ -5,15 +5,18 @@ from app.models import User,Pitch,Comments
 from flask_migrate import Migrate, MigrateCommand
 
 
-app = create_app('default')
+app = create_app('production')
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User,Pitch = Pitch, Comments = Comments )
 
 
 
